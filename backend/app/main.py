a1db358a4f596690b7f480899272b3f5ae91a004
wostibishal <bishalwasti1@gmail.com>
2026-01-09 11:42:15 +0545

    new update  with no update

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI # type: ignore
+from fastapi import FastAPI
 from sqlmodel import SQLModel
 from backend.app.db.database import engine
 from backend.app.routers import auths_router, product_router
